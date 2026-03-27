76e69d791eb7030e9b4bb171e514dc14d31da6c9
alzainnl <zainalutama01@student.ub.ac.id>
2026-03-27 20:41:55 +0700

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle{
+    Vehicle(){
+        System.out.println("Vehicle is created");
+    }
+    
+    public void drive(){
+        System.out.println("Vehicle is driving");
+    }
+    
+    public void stop(){
+        System.out.println("Vehicle is stopping");
+    }
+}
